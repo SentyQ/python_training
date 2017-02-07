3daf161120269c8d821aec85156030da20b07463
SentyQ <smarchenko@at-consulting.ru>
2017-02-07 15:42:56 +0300

    общая фикстура

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
-import unittest
-
-import pytest
 
 from model.contact import Contact
 
-from fixture.application import Application
-from model.contact import Contact
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
     
-def test_test_add_contact(app):
+def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(FirstName="NewOne", MiddleName="J", LastName="Person", Year="1899"))
     app.session.logout()
