721cf75d060fb86da611f35e417cf0fb3731b4f9
SentyQ <smarchenko@at-consulting.ru>
2017-02-07 16:48:16 +0300

    сгруппированные тесты на удаление создание и редактирование. поправлен криэйт контакт

--- fixture/contact.py
+++ fixture/contact.py
@@ -6,7 +6,7 @@ class ContactHelper:
 
     def create(self, contact):
         wd = self.wd
-        self.open_contact_page()
+        self.open_contact_add_page()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.FirstName)
@@ -16,16 +16,40 @@ class ContactHelper:
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys(contact.LastName)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[7]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[7]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[8]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[8]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys(contact.Year)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        wd.find_element_by_name("submit").click()
 
-    def open_contact_page(self):
+
+    def modify_first_contact(self, contact):
+        wd = self.wd
+        wd.find_element_by_link_text("home").click()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_link_text("Edit").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.FirstName)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.MiddleName)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.LastName)
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.Year)
+        wd.find_element_by_name("update").click()
+        wd.find_element_by_link_text("home").click()
+
+    def delete_first_contact(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home").click()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
+        wd.switch_to_alert().accept()
+        wd.find_element_by_link_text("home").click()
+
+
+
+    def open_contact_add_page(self):
         wd = self.wd
         wd.find_element_by_link_text("add new").click()
