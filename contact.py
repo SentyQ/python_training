010a53ae4fbf498a9abaafb3fe3122a9c1b21334
SentyQ <smarchenko@at-consulting.ru>
2017-02-07 10:46:19 +0300

    тест добавления контакта

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+
+    def __init__(self, FirstName, MiddleName, LastName, Year):
+        self.FirstName = FirstName
+        self. MiddleName = MiddleName
+        self.LastName = LastName
+        self.Year = Year
