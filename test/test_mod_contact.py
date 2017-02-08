7bf6a16446ce9107ba10d5f9be7420924522a34e
SentyQ <smarchenko@at-consulting.ru>
2017-02-08 16:52:30 +0300

    123йцк

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -4,4 +4,6 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
+        if app.contact.count() == 0:
+                app.contact.create(Contact(FirstName="test"))
         app.contact.modify_first_contact(Contact(FirstName="OtherOne", MiddleName="JK", LastName="2Person", Year="1999"))
