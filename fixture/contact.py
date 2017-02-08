f9a64715fcd6617f3d9a67fda52ede58028bfdbd
SentyQ <smarchenko@at-consulting.ru>
2017-02-08 18:09:15 +0300

    добавлены условия проверки что страница уже открыта

--- fixture/contact.py
+++ fixture/contact.py
@@ -36,7 +36,8 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not wd.current_url.endswith("/addressbook/") :
+            wd.find_element_by_link_text("home").click()
 
     def delete_first_contact(self):
         wd = self.app.wd
@@ -52,7 +53,8 @@ class ContactHelper:
 
     def open_contact_add_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not(wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit"))> 0) :
+            wd.find_element_by_link_text("add new").click()
 
     def count(self):
         wd = self.app.wd
