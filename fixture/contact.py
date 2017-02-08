c11796d20a0b35e382cb44ceec735b4ff5295a9c
SentyQ <smarchenko@at-consulting.ru>
2017-02-08 10:56:13 +0300

    переделал с нэйм на хпаф

--- fixture/contact.py
+++ fixture/contact.py
@@ -23,13 +23,13 @@ class ContactHelper:
         if text is not None:
             wd.find_element_by_name(field_name).click()
             wd.find_element_by_name(field_name).clear()
-            wd.find_element_by_name("firstname").send_keys(text)
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def modify_first_contact(self, contact):
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
         self.select_first_contact()
-        wd.find_element_by_link_text("Edit").click()
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
         self.fill_contact_form(contact)
         wd.find_element_by_name("update").click()
         wd.find_element_by_link_text("home").click()
@@ -38,7 +38,7 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
         self.select_first_contact()
-        wd.find_element_by_name("delete").click()
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
         wd.find_element_by_link_text("home").click()
 
