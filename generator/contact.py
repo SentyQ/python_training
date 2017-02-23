625775b65c4b5569c3fe1cb654a9fa7f231ee424
SentyQ <smarchenko@at-consulting.ru>
2017-02-23 13:45:30 +0300

    17, 18

--- generator/contact.py
+++ generator/contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 import random
 import string
-import json
+import jsonpickle
 import os.path
 import getopt
 import sys
@@ -39,4 +39,6 @@ testdata = [Contact(FirstName="", MiddleName="", LastName="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
+
